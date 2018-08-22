be19b424931f8c1d52380ce28963446474f73025
myth <myth@overflow.no>
2018-08-22 22:15:57 +0200

    Change to mini css loader and add optimization plugins

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,50 +1,84 @@
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
-module.exports = {
-  entry: "./src/app.tsx",
-  output: {
-    filename: "app.js",
-    path: __dirname + "/dist"
-  },
+module.exports = (env, argv) => {
+  // PRODUCTION will trigger optimization and compile all css into one minified file
+  const PRODUCTION = argv.mode ? argv.mode === 'production' : process.env.NODE_ENV === 'production'
 
-  // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
+  return {
+    entry: "./src/app.tsx",
+    mode: PRODUCTION ? 'production' : 'development',
 
-  resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: [".ts", ".tsx", ".js", ".json"]
-  },
+    output: {
+      filename: "app.js",
+      path: __dirname + "/dist"
+    },
 
-  module: {
-    rules: [
-      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-      {
-        test: /\.tsx?$/,
-        loader: "awesome-typescript-loader"
-      },
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
 
-      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      {
-        test: /\.js$/,
-        loader: "source-map-loader",
-        enforce: "pre",
-      },
+    resolve: {
+      // Add '.ts' and '.tsx' as resolvable extensions.
+      extensions: [".ts", ".tsx", ".js", ".json"]
+    },
 
-      // SASS / SCSS
-      {
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract("css-loader!sass-loader")
-      },
-    ]
-  },
+    module: {
+      rules: [
+        // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+        {
+          test: /\.tsx?$/,
+          loader: "ts-loader"
+        },
 
-  plugins: [
-    new ExtractTextPlugin("[name].css"),
-    new HtmlWebpackPlugin({
-      title: "Overflow",
-      template: __dirname + "/src/app.html",
-      filename: "index.html"
-    })
-  ],
+        // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+        {
+          test: /\.js$/,
+          loader: "source-map-loader",
+          enforce: "pre",
+        },
+
+        // SASS / SCSS
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            PRODUCTION ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              // Set up postcss to use the autoprefixer plugin
+              options: { plugins: () => [require('autoprefixer')] }
+            },
+            'sass-loader',
+          ],
+        },
+      ]
+    },
+
+    plugins: [
+      new MiniCssExtractPlugin({
+        // Dynamically support HRM and single file minified css
+        filename: PRODUCTION ? '[name].[hash].css' : '[name].css',
+        chunkFilename: PRODUCTION ? '[id].[hash].css' : '[id].css'
+      }),
+      new HtmlWebpackPlugin({
+        title: "Overflow",
+        template: __dirname + "/src/app.html",
+        filename: "index.html"
+      })
+    ],
+
+    // Optimizations are enabled when PRODUCTION is true
+    optimization: {
+      minimizer: [
+        new UglifyJsPlugin({
+          cache: true,
+          parallel: true,
+          sourceMap: true // Set to true if you want JS source maps
+        }),
+        new OptimizeCSSAssetsPlugin({})
+      ]
+    }
+  }
 }
