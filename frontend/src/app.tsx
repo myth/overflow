7fe90c07b98d3c0c92c08249b2ffd18fb796fb5f
myth <myth@overflow.no>
2019-04-28 00:46:41 +0200

    Fix 404 styling and add hamster

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -26,7 +26,10 @@ const NotFound: React.FunctionComponent = props => {
   return (
     <div className="row">
       <div className="col-xs-12">
-        <h4>After several milliseconds, the hamsters gave up trying to find content for this URL.</h4>
+        <div id="not-found">
+          <h4>The server hamster couldn't find anything here and gave up.</h4>
+          <img src="/images/server-hamster.jpg" id="not-found__image" />
+        </div>
       </div>
     </div>
   );
