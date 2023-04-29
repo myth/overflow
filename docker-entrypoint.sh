e073c692e3fe67096cf91d5aa4a143586836b96a
Aleksander Skraastad <myth@overflow.no>
2023-04-30 00:10:02 +0200

    Update port bind syntax after swap from daphne to uvicorn

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@
 python3 manage.py migrate --no-input
 
 # Start the application socket
-uvicorn -p 8080 overflow.asgi:application
+uvicorn --port 8080 overflow.asgi:application
 
 # Start nginx
 nginx
