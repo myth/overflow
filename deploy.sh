1e738034e80b6a8fceb9caefb9873fc467dbfb23
Aleksander Skraastad <myth@overflow.no>
2024-11-12 21:31:22 +0100

    fix: tags and container labels align (v prefix). remove old drone config file

--- deploy.sh
+++ deploy.sh
@@ -16,11 +16,11 @@ latest=$(git tag | sort -V | tail -1)
 read -p "Enter a version for this release (current is $latest): v" version
 read -p 'Enter a comment for this release: ' comment
 
-sed -i "s,myth/overflow.*,myth/overflow:$version," docker-compose.yml
+sed -i "s,myth/overflow.*,myth/overflow:v$version," docker-compose.yml
 sed -i "/^version = /s/.*/version = \"$version\"/" pyproject.toml
 uv sync
 git commit -am "chore: Prepare v${version}"
-echo "Pinned docker-compose.yml to mythern/overflow:$version and updated version in pyproject.toml"
+echo "Pinned docker-compose.yml to myth/overflow:v$version and updated version in pyproject.toml"
 git tag -a -s -m "$comment" "v$version"
 echo "Created signed tag: 'v$version' ('$comment')"
 
